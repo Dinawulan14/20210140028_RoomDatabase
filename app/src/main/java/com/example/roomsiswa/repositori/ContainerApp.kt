c04e950b494036c812ef9a7cabab85f1537bc84a
TA1028 <dina.wulan.ft21@mail.umy.ac.id>
2023-12-05 19:54:12 +0700

    Initial commit

---
+++ app/src/main/java/com/example/roomsiswa/repositori/ContainerApp.kt
@@ -0,0 +1,16 @@
+package com.example.roomsiswa.repositori
+
+import android.content.Context
+import com.example.roomsiswa.data.DatabaseSiswa
+import com.example.rootsiswa.repositori.RepositoriSiswa
+
+
+interface ContainerApp {
+    val repositoriSiswa : RepositoriSiswa
+}
+
+class ContainerDataApp(private val context: Context): ContainerApp {
+    override val repositoriSiswa: RepositoriSiswa by lazy {
+        OfflineRepositoriSiswa(DatabaseSiswa.getDatabase(context).siswaDao())
+    }
+}
