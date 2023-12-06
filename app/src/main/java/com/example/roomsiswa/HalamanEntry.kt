9a1b1bdd95eb676f9ef025eefceac9f2fd8d94a5
TA1028 <dina.wulan.ft21@mail.umy.ac.id>
2023-12-06 11:22:08 +0700

    Membuat fun EntrySiswaScreen

--- app/src/main/java/com/example/roomsiswa/HalamanEntry.kt
+++ app/src/main/java/com/example/roomsiswa/HalamanEntry.kt
@@ -31,3 +31,6 @@ object DestinasiEntry: DestinasiNavigasi {
     override val route = "item_entry"
     override val titleRes = R . string . entry_siswa
 }
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntrySiswaScreen(){}
